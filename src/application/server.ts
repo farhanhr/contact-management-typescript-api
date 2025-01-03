b41671732e957e1894cdd39f6776a5e49aa67bf4
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-01-03 21:15:20 +0700

    setup router for user register

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { publicRouter } from "../router/public-api";
 
 export const server = express();
 server.use(express.json());
+
+server.use(express.json());
+server.use(publicRouter);
