f4460ca9769e0bd003be56d9e15afb1ec5b99342
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-01-03 21:24:30 +0700

    add error middleware

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { publicRouter } from "../router/public-api";
+import { errorMiddleware } from "../middleware/error-middleware";
 
 export const server = express();
 server.use(express.json());
 
 server.use(express.json());
 server.use(publicRouter);
+server.use(errorMiddleware);
