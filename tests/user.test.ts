527712521b4de0b89a5721e7980b1fe84a91dc07
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-01-04 10:00:17 +0700

    add login api and test

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -41,3 +41,43 @@ describe('Post /api/users', () => {
         });
 
 });
+
+describe('POST /api/users/login', () => {
+
+    beforeEach(async () => {
+        await UserTest.create();
+    });
+
+    afterEach(async () => {
+        await UserTest.delete();
+    });
+
+    it('should be able to login', async () => {
+        const response = await supertest(server)
+            .post("/api/users/login")
+            .send({
+                username: "test",
+                password: "test"
+            });
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+        expect(response.body.data.username).toBe("test");
+        expect(response.body.data.name).toBe("test");
+        expect(response.body.data.token).toBeDefined();
+    });
+
+
+    it('should reject login when username or password is wrong', async () => {
+        const response = await supertest(server)
+            .post("/api/users/login")
+            .send({
+                username: "test",
+                password: "wrong password"
+            });
+
+        logger.debug(response.body);
+        expect(response.status).toBe(401);
+        expect(response.body.errors).toBeDefined();
+    });
+});
