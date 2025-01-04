46b4a371edf3af5390141629ad5b2be8ad598230
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-01-04 14:45:24 +0700

    add logout api

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -56,4 +56,15 @@ export class UserController {
         }
     }
     
+    static async logout(req: UserRequest, res: Response, next: NextFunction) {
+        try {
+            const response = await UserService.logout(req.user!);
+            res.status(200).json({
+                data: response
+            });
+        }
+        catch(e) {
+            next(e);
+        }
+    }
 }
