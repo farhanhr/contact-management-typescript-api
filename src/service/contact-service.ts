e52e929f8338cb7b792ee774ed0a32547d5e1405
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-01-04 16:44:08 +0700

    add getContact api

--- src/service/contact-service.ts
+++ src/service/contact-service.ts
@@ -3,11 +3,11 @@ import { ContactResponse, CreateContactRequest, toContactResponse } from "../mod
 import { ContactValidation } from "../validation/contact-validation";
 import { Validation } from "../validation/validation";
 import { prismaClient } from "../application/database";
-import { logger } from "../application/logging";
+import { ResponseError } from "../error/response-error";
 
 export class ContactService {
 
-    static async create(user: User,request: CreateContactRequest): Promise<ContactResponse> {
+    static async create(user: User, request: CreateContactRequest): Promise<ContactResponse> {
         const createRequest = Validation.validate(ContactValidation.CREATE, request);
 
         const record = {
@@ -18,6 +18,21 @@ export class ContactService {
         const contact = await prismaClient.contact.create({
             data: record
         });
+
+        return toContactResponse(contact);
+    }
+
+    static async get(user: User, id: number): Promise<ContactResponse> {
+        const contact = await prismaClient.contact.findUnique({
+            where: {
+                id: id,
+                username: user.username
+            }
+        });
+
+        if(!contact) {
+            throw new ResponseError(404, "Contact not found");
+        }
 
         return toContactResponse(contact);
     }
